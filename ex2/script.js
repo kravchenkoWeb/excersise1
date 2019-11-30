3c7bd55722fe850bd1885c757e671357d5346dcb
user.name <kravchenko.web@gmail.com>
2019-11-30 11:14:18 +0300

    Added ex2 folder with completed assigment

--- ex2/script.js
+++ ex2/script.js
@@ -24,6 +24,34 @@ for (let i = 0; i < 2; i++) {
     
 };
 
+// while(i < 2) {
+//     let a = prompt('Введите обязательную статью расходов в єтом месяце', ""),
+//         b = prompt('Во сколько обойдеться?', "");
+
+//     if( (typeof(a) ) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b!='' && a.length < 50 ) {
+//         console.log("done");
+//         appData.expences[a] = b;
+//     }
+//     else {
+
+//     }
+    // i++;
+// }
+
+// do {
+//    //     let a = prompt('Введите обязательную статью расходов в єтом месяце', ""),
+// //         b = prompt('Во сколько обойдеться?', "");
+
+// //     if( (typeof(a) ) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b!='' && a.length < 50 ) {
+// //         console.log("done");
+// //         appData.expences[a] = b;
+// //     }
+// //     else {
+
+// //     }
+//     // i++; 
+// } while(i<2);
+
 appData.moneyPerDay = appData.budget / 30;
 
 if(appData.moneyPerDay < 100) {
