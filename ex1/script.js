fb5ac5a71f44224f085fdbc9a8a983424e2c3257
user.name <kravchenko.web@gmail.com>
2019-11-30 10:42:34 +0300

    Changed code in addition to video example

--- ex1/script.js
+++ ex1/script.js
@@ -1,24 +1,19 @@
-var money = prompt("Ваш бюджет на месяц?", "10000"),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "2019-11-29"),
-    howMuch_first = prompt('Введите обязательную статью расходов в этом месяце'),
-    whatTakes_first = prompt('Во сколько обойдется?'),
-    howMuch_second = prompt('Введите обязательную статью расходов в этом месяце'),
-    whatTakes_second = prompt('Во сколько обойдется?');
-    appData = {
-        budget: +money,
-        timeData: time,
-        expenses: {
-            howMuch_first: whatTakes_first,
-            howMuch_second: whatTakes_second
-        }
-    };
- 
-var budgetOneDay = Math.round(appData.budget / 30);
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
-if(budgetOneDay <= 333) {
-    alert("Ваш бюджет на 1 день " + budgetOneDay + " не густо(");    
-}
-else {
-    alert("Ваш бюджет на 1 день " + budgetOneDay + " так то лучше");  
-}
+   let appData = {
+    budget: money,
+    timeData: time,
+    expences: {},
+    optionalExpences: {},
+    income: [],
+    savings: false
+   }; 
 
+   let a1 = prompt('Введите обязательную статью расходов в єтом месяце', ""),
+       a2 = prompt('Во сколько обойдеться?', ""),
+       a3 = prompt('Введите обязательную статью расходов в єтом месяце', ""),
+       a4 = prompt('Во сколько обойдеться?', "");
+
+appData.expences.a1 = a2;
+appData.expences.a3 = a4;    
